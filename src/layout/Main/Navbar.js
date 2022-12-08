2c4aa66fdf6e10d74a1fbf1d52004ba6aa42b866
nahid <sultannahid38@gmail.com>
2022-12-08 18:53:23 +0800

    navbar added

--- src/layout/Main/Navbar.js
+++ src/layout/Main/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-
+        <div className="navbar bg-base-200 px-10">
+            <div className="flex-1">
+                <Link className="btn btn-ghost normal-case text-xl">Content</Link>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal p-0">
+                    <li><Link>Home</Link></li>
+                    <li>
+                        <Link>About</Link>
+                    </li>
+                    <li><Link>Reading</Link></li>
+                </ul>
+            </div>
         </div>
     );
 };
